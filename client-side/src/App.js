f3d2171e959ff154d7defd9453e85a25eee4d7cf
ashishbct <ashish.vishwakarma@gravityilabs.com>
2024-09-20 17:35:56 +0530

    homePage

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,11 +1,14 @@
 // src/App.js
 
+import Home from "./components/home/Home";
+
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
         Welcome to Animore Care!
       </h1>
+      <Home/>
     </div>
   );
 }
